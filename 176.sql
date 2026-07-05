7b23469401c90be50e4d50519b95e2c90ef5b08c
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-07-05 17:16:37 +0530

    Add SQL query for second highest salary

--- 176.sql
+++ 176.sql
@@ -1,1 +1,16 @@
 select distinct max(salary) as SecondHighestSalary from Employee where salary < (select max(salary) from Employee);
+
+//2nd approach
+SELECT IFNULL(
+    (
+        SELECT salary
+        FROM (
+            SELECT salary,
+                   DENSE_RANK() OVER (ORDER BY salary DESC) AS dr
+            FROM Employee
+        ) t
+        WHERE dr = 2
+        LIMIT 1
+    ),
+    NULL
+) AS SecondHighestSalary;
