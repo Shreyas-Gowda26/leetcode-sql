6c79559d9d3e05baa394490f693fda1bacc61057
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-07-08 11:12:49 +0530

    Create 184.sql

---
+++ 184.sql
@@ -0,0 +1,1 @@
+select Department , Employee,Salary from (select d.name as Department, e.name as Employee,e.salary as Salary, dense_rank() over (partition by d.id order by e.salary desc) as dr from Employee e join Department d on e.departmentId = d.id)t where dr=1;
