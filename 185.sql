b13eafff5558331ce0d404e9bf74cd78e097c976
Shreyas G Gowda <shreyasg2526@gmail.com>
2026-07-11 10:57:59 +0530

    Create 185.sql

---
+++ 185.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+SELECT
+    Department,
+    Employee,
+    Salary
+FROM (
+    SELECT
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        DENSE_RANK() OVER (
+            PARTITION BY d.id
+            ORDER BY e.salary DESC
+        ) AS dr
+    FROM Employee e
+    JOIN Department d
+        ON e.departmentId = d.id
+) t
+WHERE dr <= 3;
